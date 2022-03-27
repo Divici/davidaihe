d8d18a9d5a9939f1d58291f5d26fa7571aaadaf5
david-aihe <doa9200@gmail.com>
2022-03-27 02:32:59 -0400

    contact functional

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 import './Contact.css'
 import {HiOutlineMail} from 'react-icons/hi'
 import {RiMessengerLine} from 'react-icons/ri'
 
+
 const Contact = (props) =>{
+    const form =useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
     
+        emailjs.sendForm('service_zx57bgm', 'template_xw6b998', form.current, 'JkQffBMfAuAOH6BAK')
+          
+        e.target.reset()
+      };
+
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -25,7 +35,7 @@ const Contact = (props) =>{
                         <a href="https://m.me/daihe" target='_blank' rel="noreferrer">Send a Message</a> 
                     </article>
                 </div>
-                <form action=''>
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Your Full Name' required />
                     <input type="email" name='email' placeholder='Your Email' required />
                     <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
